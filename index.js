0c6c46c2b05c4efd62079c9bc5ad72354ddaddd8
aharris88 <aharris88@gmail.com>
2014-05-28 13:36:00 -0600

    Add comments for each problem

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Problem 1
 function identity(a) {
   return a;
 }
 
+// Problem 2
 function add(a, b) {
   return a + b;
 }
@@ -10,18 +12,21 @@ function mul(a, b) {
   return a * b;
 }
 
+// Problem 3
 function identifyf(a) {
   return function() {
     return a;
   };
 }
 
+// Problem 4
 function addf(a) {
   return function(b) {
     return a + b;
   };
 }
 
+// Problem 5
 function applyf(binaryFunction) {
   return function(a) {
     return function(b) {
@@ -30,6 +35,7 @@ function applyf(binaryFunction) {
   };
 }
 
+// Problem 6
 function curry(binaryFunction, a) {
   return function(b) {
     return binaryFunction(a, b);
@@ -40,6 +46,7 @@ function curry(binaryFunction, a) {
   return applyf(binaryFunction)(a);
 }
 
+// Problem 7
 inc = addf(1);
 inc = applyf(add)(1);
 inc = curry(add, 1);
